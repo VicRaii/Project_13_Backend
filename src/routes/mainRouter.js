7197a592e4977e0026ee10f568827d3f9b8a17c0
VicRaii <vimarupon@hotmail.com>
2025-04-30 13:17:26 +0200

    delete event CRUD and add isAuth to getPreachings

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,13 +1,11 @@
 const usersRouter = require('./authRoutes')
 const contactRouter = require('./contactRoutes')
-const eventsRouter = require('./eventRoutes')
 const preachingsRouter = require('./preachingRoutes')
 const seriesRouter = require('./seriesRoutes')
 
 const mainRouter = require('express').Router()
 
 mainRouter.use('/users', usersRouter)
-mainRouter.use('/events', eventsRouter)
 mainRouter.use('/preachings', preachingsRouter)
 mainRouter.use('/series', seriesRouter)
 mainRouter.use('/contact', contactRouter)
