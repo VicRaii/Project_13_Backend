bd79793ddf5dfd5810b9564fc1ef4337c674835f
VicRaii <vimarupon@hotmail.com>
2025-04-30 13:10:58 +0200

    delete isAuth to getPreachings

--- src/routes/preachingRoutes.js
+++ src/routes/preachingRoutes.js
@@ -8,7 +8,7 @@ const { isAuth } = require('../middleware/auth')
 
 const preachingsRouter = require('express').Router()
 
-preachingsRouter.get('/', [isAuth], getPreachings)
+preachingsRouter.get('/', getPreachings)
 preachingsRouter.post('/', [isAuth], createPreaching)
 preachingsRouter.put('/:id', [isAuth], updatePreaching)
 preachingsRouter.delete('/:id', [isAuth], deletePreaching)
